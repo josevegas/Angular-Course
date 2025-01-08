907b9c2a3970209713762a49ebc58b6cd74a2dc8
joseluisvegas <161845982+joseluisvegas@users.noreply.github.com>
2025-01-07 23:57:02 -0500

    avance de curso de Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './pages/todo/todo.component';
+import { TODO_DATA } from '../assets/todo';
+import { NTodo } from './models/todo.model';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,TodoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  todoData=TODO_DATA;
+  //todoData:NTodo.TodoData[]=[];
+  getTodoInfo(val:NTodo.TodoData){
+    console.log(val);
+  }
+  orderData(){
+    this.todoData.sort((a,b)=>a.priority-b.priority)
+  }
+}
---
+++ src/app/pages/todo/todo.component.ts
@@ -0,0 +1,53 @@
+import { Component, EventEmitter, Input, LOCALE_ID, Output, Pipe } from '@angular/core';
+import { NTodo } from '../../models/todo.model';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import spanish from '@angular/common/locales/es';
+registerLocaleData(spanish)
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es'
+    }
+  ],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.scss'
+})
+export class TodoComponent {
+  @Input({required: true}) todoData!: NTodo.TodoData;
+  @Input() first!:boolean;
+  @Input() last!:boolean;
+  @Input() odd!: boolean;
+  @Input() even!: boolean;
+  @Output() onClickIcon=new EventEmitter<NTodo.TodoData>();
+  get priority():string{
+    switch (this.todoData.priority) {
+      case NTodo.Priority.LOW:
+        return NTodo.PriorityText.LOW
+        break;
+      case NTodo.Priority.MEDIUM:
+        return NTodo.PriorityText.MEDIUM;
+        break;
+          
+      default:
+        return NTodo.PriorityText.HIGH;
+        break;
+    }
+    
+  }
+  get progress(){
+    return this.todoData.progress*100;
+  }
+  get range(){
+    if(this.progress>0 && this.progress<=NTodo.Range.LOW){
+      return NTodo.RangeText.LOW
+    }else if(this.progress>NTodo.Range.LOW && this.progress<=NTodo.Range.MEDIUM){
+      return NTodo.RangeText.MEDIUM;
+    }else{
+      return NTodo.RangeText.HIGH;
+    }
+  }
+}
